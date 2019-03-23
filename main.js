ffde123a23e5312346010303b7a90aac9f236d13
korynewton <korynewtondev@gmail.com>
2019-03-23 15:59:23 -0700

    electron computer info app

---
+++ main.js
@@ -0,0 +1,36 @@
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+const url = require('url');
+
+
+let win;
+
+function createWindow() {
+    //create browser window
+    win = new BrowserWindow({width:800, height:600, icon:__dirname+'/img/gear.png'}); 
+
+    //load index.html
+    win.loadURL(url.format({
+        pathname: path.join(__dirname, 'index.html'),
+        protocol: 'file',
+        slashes: true
+    }));
+
+    //open devtools
+    win.webContents.openDevTools();
+
+    win.on('closed', () => {
+        win = null;
+    })
+}
+
+//run create window function on startup
+app.on('ready', createWindow);
+
+//quit when all windows are closed
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin'){
+        app.quit();
+    }
+})
+
